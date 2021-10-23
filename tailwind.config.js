495cc7acd4cc732ca8b330c68ae04d4108b47b27
Dasacav3 <dscv3719@gmail.com>
2021-10-23 14:15:56 -0500

    Added custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,24 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				danger: "#540804",
+				midnight_blue: "#083854",
+				orange: "#E54C2F",
+				gold_yellow: "#EE8933",
+				light_blue: "#00B0FF",
+				aquamarine_blue: "#47A8BD",
+				orange_light: "#FF847A",
+				gold_yellow_light: "rgba(238, 137, 51, 0.59)",
+				blue_grey: "rgba(7, 50, 78, 0.72)",
+				dark_grey: "#535461",
+				light_grey: "#C4C4C4",
+			},
+			backgroundImage: {
+				midnight_dark_blue: "linear-gradient(213.31deg, #052C48 11.43%, rgba(5, 44, 72, 0) 91.27%)",
+				aquamarine_blue_light: "linear-gradient(180deg, #4DB6AC 0%, rgba(77, 182, 172, 0) 100%)",
 			},
 			fontFamily: {
 				bahnscrift: ["Bahnschrift", "sans-serif"],
+				raleway: ["Raleway", "sans-serif"],
 			},
 		},
 	},
