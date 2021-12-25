76b824b02fcfd04114ed5aef74a14550de25ab5a
Dasacav3 <dscv3719@gmail.com>
2021-12-25 11:17:50 -0500

    Added new routes

--- routes/router.js
+++ routes/router.js
@@ -4,5 +4,16 @@ const router = express.Router();
 router.get("/", (req, res) => {
 	res.render("index");
 });
+router.get("/development", (req, res) => {
+	res.render("development");
+});
+
+router.get("/blog", (req, res) => {
+	res.render("blog");
+});
+
+router.get("/contact", (req, res) => {
+	res.render("contact");
+});
 
 module.exports = { router };
