919acc01289a4e38541463b914db21c7c55750ba
Dasacav3 <dscv3719@gmail.com>
2021-12-25 11:20:37 -0500

    Added mongoose connection to MongoDB

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Connection to DB MongoDB Atlas
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.peqdu.mongodb.net/${process.env.MONGODB_DB}?retryWrites=true&w=majority`;
+
+mongoose
+	.connect(uri, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log("Base de datos conectada"))
+	.catch((e) => console.error(e));
 
 // Express middlewares
 app.use(express.urlencoded({ extended: true }));
@@ -18,6 +33,8 @@ app.set("views", __dirname + "/views");
 
 // Web Routes
 app.use("/", require("./routes/router").router);
+
+app.use("/projects", require("./routes/projects").router);
 
 // Set 404 error
 app.use((req, res, next) => {
